4bda9012692d153d42ebbc8535cc3a1badd934c6
OrkunSA <c.orkun.s@gmail.com>
2020-11-26 13:50:13 -0600

    rendering the books in DOM

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+const api = new ApiService;
+
+document.addEventListener("DOMContentLoaded", () => {
+    api.fetchBooks()
+    .then(books => {
+        books.forEach(book => {
+            const newBook = new Books(book)
+            newBook.renderBooks()
+        })
+    })
+})
